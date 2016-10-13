ea04e8d2380298792312bb107c0fcc8b7db65c24
jimmy-sonny <s209051@studenti.polito.it>
2016-10-14 00:18:25 +0200

    Added second script to plot data

--- __utility/finalizer.sh
+++ __utility/finalizer.sh
@@ -9,6 +9,13 @@ fi
 echo "# I will parse this file: " $1
 
 line_number=`cat $1 | grep -n "Evolution terminated" | cut -f1 -d":"`
-end_of_file=`wc -l $1 | sed "s/[$1,' ']//g"`
+echo "start_line:" $line_numberend_of_file
+end_of_file=`wc -l $1 | sed 's/ //g' | sed "s/$1//g"`
+echo "end_line:" $end_of_file
+
 sed -n $(($line_number+2)),$(($end_of_file-1))p $1 | cut -f2 -d "{" | sed 's/}//g' > data.txt
 python3 plotter.py data.txt plot1.png plot2.png
+
+
+cat $1 | grep "^0." > data2_t.txt
+python3 plotter2.py data.txt data2_t.txt plot1_t.png plot2_t.png
---
+++ __utility/plotter2.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import sys
+
+if len(sys.argv) != 5:
+    print("The number of supplied arguments is wrong")
+    sys.exit(1)
+else:
+    print("## I will use this data file: " + str(sys.argv[1]) + " " + sys.argv[2])
+    print("## I will plot a graph with this name: " + str(sys.argv[3]) + " " + str(sys.argv[4]))
+
+def getColumn(filename, column, delimiter_input):
+    results = csv.reader(open(filename), delimiter=delimiter_input)
+    return [result[column] for result in results]
+
+try:
+    hd = [np.absolute(np.log(float(i))) for i in getColumn(sys.argv[1], 0, " ")]
+    sr = [np.absolute(np.log(float(i))) for i in getColumn(sys.argv[1], 1, " ")]
+    length = [1/(float(i)) for i in getColumn(sys.argv[1], 2, " ")]
+
+    hd_t = [np.absolute(np.log(float(i))) for i in getColumn(sys.argv[2], 0, " ")]
+    sr_t = [np.absolute(np.log(float(i))) for i in getColumn(sys.argv[2], 1, " ")]
+    length_t = [1/(float(i)) for i in getColumn(sys.argv[2], 2, " ")]
+
+    plt.xlabel("Length")
+    plt.ylabel("HD")
+
+    plt.plot(length_t, hd_t, 'rs', length, hd, 'bs')
+    plt.savefig(sys.argv[3])
+    # plt.show()
+
+    plt.clf()
+
+    plt.xlabel("Length")
+    plt.ylabel("Rare Signals")
+
+    plt.plot(length_t, sr_t, 'rs', length, sr, 'bs')
+    plt.savefig(sys.argv[4])
+    # plt.show()
+
+    sys.exit(0)
+except Exception as e:
+    print("Sorry, something went wrong! (Maybe input files does not exist)")
+    print(e)
+    sys.exit(1)
